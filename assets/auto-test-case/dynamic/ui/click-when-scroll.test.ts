624b1c8bbd3d99a15e036b957bfbc0369d991694
sushanhong <379308184@qq.com>
2024-08-02 18:29:09 +0800

    Autest ssh v3.8.4 (#876)
    * Update click-when-scroll.test.ts
    
    update screen time
    
    * Update terrain.test.ts
    
    update screen time

--- assets/auto-test-case/dynamic/ui/click-when-scroll.test.ts
+++ assets/auto-test-case/dynamic/ui/click-when-scroll.test.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { testCase, testClass, waitForFrames } from 'db://automation-framework/runtime/test-framework.mjs';
+import { testCase, testClass, waitForFrames, waitForNextFrame } from 'db://automation-framework/runtime/test-framework.mjs';
 import { screenshot_custom_by_wait } from '../common/utils';
 import { find, Button } from 'cc';
 import { simulateTouchStart, simulateTouchEnd, simulateTouchMove, UISimulate } from '../common/SimulateEvent';
@@ -15,10 +15,12 @@ export class ClickWhenScroll {
     async clickItem() {
         const contentNode = find('Canvas/New ScrollView/view/content')!;
 
-        for (let item, i=0; i<7; i++) {
+        for (let item, i = 0; i < 7; i++) {
             if (i === 5) {
                 simulateTouchStart(0, 0, contentNode);
+                await waitForNextFrame(30);
                 simulateTouchMove(contentNode, 0, 200);
+                await waitForNextFrame(30);
                 simulateTouchEnd(contentNode, 0, 200);
                 await waitForFrames(60);
             }
--- assets/auto-test-case/staticScene/terrain.test.ts
+++ assets/auto-test-case/staticScene/terrain.test.ts
@@ -1,14 +1,14 @@
 // @ts-ignore
 import { Label, Toggle, find, sys } from 'cc';
-import { captureOneImage, waitForNextFrame, runScene, testCase, testClass, expect } from 'db://automation-framework/runtime/test-framework.mjs';
+import { testCase, testClass, expect } from 'db://automation-framework/runtime/test-framework.mjs';
+import { screenshot_custom_by_wait } from '../dynamic/common/utils';
 
 @testClass('Terrain', 'terrain')
 export class Terrain {
 
     @testCase
     async startPlay() {
-        await waitForNextFrame();
-        await captureOneImage();
+        await screenshot_custom_by_wait(60);
     }
 
     @testCase
@@ -22,14 +22,12 @@ export class Terrain {
         const value1 = parseInt(label.string);
 
         toggle.isChecked = true;
-        await waitForNextFrame(30);
-        await captureOneImage();
+        await screenshot_custom_by_wait(90);
 
         const value2 = parseInt(label.string);
 
         toggle.isChecked = false;
-        await waitForNextFrame(30);
-        await captureOneImage();
+        await screenshot_custom_by_wait(90);
 
         if (sys.platform === sys.Platform.DESKTOP_BROWSER || sys.platform === sys.Platform.MOBILE_BROWSER) {
             expect(value2, `勾选 Lod 后 Triangle（数值=${value2}）应比未勾选的（数值=${value1}）小`).to.be.below(value1);
